f7d91bf456c1c63a2d8ff51b714787f1b7b9249b
kimbab2066 <qseft523125@g.yju.ac.kr>
2025-09-11 16:17:41 +0900

    condition in js

--- public/js/index.js
+++ public/js/index.js
@@ -8,3 +8,26 @@ function show_info() {
 function changeH3() {
   document.querySelector("h3#hello").innerHTML = "Yes";
 }
+function changeValue() {
+  let score = document.querySelector("#user_value").value;
+  score = parseInt(score);
+  let res;
+  console.log(score);
+  switch (true) {
+    case score >= 90:
+      res = "very good";
+      break;
+    case score >= 80:
+      res = "good";
+      break;
+    case score >= 70:
+      res = "soso";
+      break;
+    case score >= 60:
+      res = "bad";
+      break;
+    default:
+      res = "Fail";
+  }
+  console.log(res);
+}
